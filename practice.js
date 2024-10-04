25a63e68dcabbce1bab4c9bd48607bcca10b4714
vikagladka <vikagladka@ukr.net>
2024-10-04 04:10:20 +0300

    Add loop exersices

--- practice.js
+++ practice.js
@@ -442,6 +442,47 @@ calculator(5, 2, '-');
 
 
 
+// Instructions:
+
+// . Print/log the numbers from 1 to 100
+// . For multiples of three print "Fizz" instead of the
+// number
+// . For multiples of five print "Buzz"
+// . For numbers which are multiples of both three and
+// five print "FizzBuzz". 
+
+// варіант 1 рішення:
+
+for (let i = 1; i <= 100; i++) {
+ 
+  if (i % 3 === 0 && i % 5 === 0){
+      console.log('Fizz');
+  } else if(i % 5 ===0){
+    console.log('Buzz');
+  }else if(i % 3 === 0){
+     console.log('FizzBuzz');
+  } else{ console.log(i);
+  };
+}
+
+// варіант 2 рішення:
+
+
+for (let i = 1; i <= 100; i++) {
+  let output = (i % 3 === 0 && i % 5 === 0) ? 'FizzBuzz' :
+               (i % 3 === 0) ? 'Fizz' :
+               (i % 5 === 0) ? 'Buzz' : i;
+  console.log(output);
+}
+
+
+// Приклад циклу  for ... in
+
+const object = {a: 1, b: 2, c: 3};
+
+for (const key in object) {
+  console.log(`${key}: ${object[key]}`);
+}
 
 
 
